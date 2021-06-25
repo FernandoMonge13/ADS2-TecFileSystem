6c5cfb6bc9be9bd67987b853beeaae24a14468db
Fernando <fercoche369@gmail.com>
2021-06-24 18:46:24 -0600

    Final Huffman

--- Huffman/Huffman.cpp
+++ Huffman/Huffman.cpp
@@ -4,29 +4,128 @@
 
 #include "Huffman.h"
 
+Huffman* Huffman:: instance = nullptr;
+
+
 
 Huffman::Huffman() {
 
 }
 
-struct TreeNode{
+struct MinHeapNode{
     char data;
-    int rep;
-    TreeNode *right;
-    TreeNode *left;
+    int freq;
+    MinHeapNode *left, *right;
 
-    TreeNode(char data, int rep){
-        right = NULL;
-        left = NULL;
+    MinHeapNode(char data, int freq){
+        left = right = NULL;
         this->data = data;
-        this->rep = rep;
+        this->freq = freq;
     }
 };
+
 
 struct compare{
-    bool operator()(TreeNode* x, TreeNode* y){
-        return (x->rep > y->rep);
+    bool operator()(MinHeapNode* l, MinHeapNode* r){
+        return (l->freq > r->freq);
+    }
+};
+
+//void Huffman::printCodes(struct MinHeapNode *root, std::string str) {
+//    if (!root)
+//        return;
+//    if (root->data != '$')
+//        std::cout << root->data << ": " << str << "\n";
+//    printCodes(root->left, str + "0");
+//    printCodes(root->right, str + "1");
+//
+//}
+
+void Huffman::storeCodes(struct MinHeapNode *root, std::string str) {
+    if (root==NULL){
+        return;
     }
 
+    if (root->data != '$'){
+        codes[root->data]=str;
+    }
+    storeCodes(root->left, str + "0");
+    storeCodes(root->right, str + "1");
+}
 
-};
+std::priority_queue<MinHeapNode*, std::vector<MinHeapNode*>, compare> minHeap;
+
+void Huffman::HuffmanCodes(int size) {
+
+    struct MinHeapNode *left, *right, *top;
+    for (std::map<char, int>::iterator v=freq.begin(); v!=freq.end(); v++){
+        minHeap.push(new MinHeapNode(v->first, v->second));
+    }
+
+    while (minHeap.size() != 1){
+        left = minHeap.top();
+        minHeap.pop();
+        right = minHeap.top();
+        minHeap.pop();
+        top = new MinHeapNode('$', left->freq + right->freq);
+        top->left = left;
+        top->right = right;
+        minHeap.push(top);
+    }
+    storeCodes(minHeap.top(), "");
+}
+
+void Huffman::calcFreq(std::string str, int n) {
+    for (int i=0; i<str.size(); i++){
+        freq[str[i]]++;
+    }
+}
+
+std::string Huffman::decode_file(struct MinHeapNode *root, std::string s) {
+
+    std::string ans = "";
+    struct MinHeapNode* curr = minHeap.top();
+    for (int i=0;i<s.size();i++){
+        if (s[i] == '0'){
+            curr = curr->left;
+        }
+        else{
+            curr = curr->right;
+        }
+
+        if (curr->left==NULL and curr->right==NULL){
+            ans += curr->data;
+            curr = root;
+        }
+    }
+    return ans;
+}
+
+std::string Huffman::code(std::string info) {
+    std::string encodedString, decodedString;
+    calcFreq(info, info.length());
+    HuffmanCodes(info.length());
+//    std::cout << "Character With there Frequencies:\n";
+//    for (auto v=codes.begin(); v!=codes.end(); v++)
+//        std::cout << v->first <<' ' << v->second << std::endl;
+
+    for (auto i: info){
+        encodedString+=codes[i];
+    }
+    return encodedString;
+
+}
+
+Huffman *Huffman::getInstance() {
+    if (instance == nullptr){
+        instance = new Huffman;
+    }
+    return instance;
+
+}
+
+std::string Huffman::decode(std::string info) {
+
+    std::string encodedString = decode_file(minHeap.top(), info);
+    return encodedString;
+}
--- Huffman/Huffman.h
+++ Huffman/Huffman.h
@@ -2,26 +2,35 @@
 // Created by fernando on 23/6/21.
 //
 
-#ifndef ADS2_TECFILESYSTEM_HUFFMAN_H
-#define ADS2_TECFILESYSTEM_HUFFMAN_H
-#define Tree_High 256
-
+#ifndef TEST_HUFFMAN_H
+#define TEST_HUFFMAN_H
+#include <iostream>
 #include <bits/stdc++.h>
 
 
 class Huffman {
-
 public:
+
     Huffman();
 
-private:
-    std::map<char, std::string> code;
-    std::map<char, int> rep;
+    static Huffman* getInstance();
+    std::map<char, std::string> codes;
+    std::map<char, int> freq;
+    std::string code(std::string info);
+    std::string decode(std::string info);
 
-    void printCode(struct TreeNode* root, std::string string);
+private:
+
+    static Huffman* instance;
+    void printCodes(struct MinHeapNode* root, std::string str);
+    void storeCodes(struct MinHeapNode* root, std::string str);
+    void HuffmanCodes(int size);
+    void calcFreq(std::string str, int n);
+    std::string decode_file(struct MinHeapNode* root, std::string s);
 
 
 };
+
 
 
 #endif //ADS2_TECFILESYSTEM_HUFFMAN_H
